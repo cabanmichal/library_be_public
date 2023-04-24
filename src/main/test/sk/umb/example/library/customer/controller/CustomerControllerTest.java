3fba7d3be5c5860324c0c769e886fdde81069c80
Robert Gallas <robert.gallas2@globallogic.com>
2023-04-24 18:43:04 +0200

    test fixes

---
+++ src/main/test/sk/umb/example/library/customer/controller/CustomerControllerTest.java
@@ -0,0 +1,57 @@
+package sk.umb.example.library.customer.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import sk.umb.example.library.authentication.core.WebSecurityConfig;
+import sk.umb.example.library.customer.service.CustomerDetailDto;
+import sk.umb.example.library.customer.service.CustomerService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CustomerController.class)
+class CustomerControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    // NOTE: @MockBean is a spring class. Not a Mockito class
+    @MockBean
+    private CustomerService customerService;
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    @WithMockUser(roles="USER")
+    public void searchCustomer() throws Exception {
+        CustomerDetailDto customerDetailDto = new CustomerDetailDto();
+
+        customerDetailDto.setFirstName("John");
+
+        when(customerService.getAllCustomers())
+                .thenReturn(List.of(customerDetailDto));
+
+        var result = mockMvc.perform(
+                get("/api/customers"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].firstName", Matchers.equalTo("John")));
+
+        verify(customerService, times(1)).getAllCustomers();
+    }
+}
---
+++ src/main/test/sk/umb/example/library/customer/service/CustomerServiceTest.java
@@ -0,0 +1,53 @@
+package sk.umb.example.library.customer.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sk.umb.example.library.address.persistence.entity.AddressEntity;
+import sk.umb.example.library.address.persistence.repository.AddressRepository;
+import sk.umb.example.library.customer.persistence.entity.CustomerEntity;
+import sk.umb.example.library.customer.persistence.repository.CustomerRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CustomerServiceTest {
+    @InjectMocks
+    private CustomerService customerService;
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @Mock
+    private AddressRepository addressRepository;
+
+    @Test
+    public void getAllCustomersOk() {
+        CustomerEntity customerEntity = new CustomerEntity();
+        customerEntity.setId(1L);
+        customerEntity.setEmailContact("emai@test.com");
+        customerEntity.setFirstName("John");
+        customerEntity.setLastName("tester");
+
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setCity("Banska Bystrica");
+        addressEntity.setId(2l);
+
+        customerEntity.setAddress(addressEntity);
+
+        when(customerRepository.findAll())
+                .thenReturn(List.of(customerEntity));
+
+        List<CustomerDetailDto> customers = customerService.getAllCustomers();
+
+        verify(customerRepository, times(1)).findAll();
+
+        assertEquals(customers.size(), 1);
+        assertEquals(customers.get(0).getEmailContact(), "emai@test.com");
+    }
+}
